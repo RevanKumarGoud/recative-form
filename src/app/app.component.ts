1b7fa8b7e93e3a58344cb0a7dd036fe9bace4796
[RevankumarGoud] <[revankumargoudkurmindla@gmail.com]>
2022-07-28 02:25:35 +0530

    REACTIVE FORM

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recative-form';
+  registationForm = new FormGroup({
+    UserName : new FormControl(''),
+    Password : new FormControl(''),
+    ConfirmPassword : new FormControl(''),
+    address: new FormGroup({
+      city : new FormControl(''),
+      State: new FormControl(''),
+      PostalCode : new FormControl ('')
+    })
+  });
+  loadApiData(){
+    this.registationForm.setValue({
+      UserName: 'Vikram',
+      Password: 'testqw',
+      ConfirmPassword: 'testqw',
+      address: {
+        city : 'city',
+        State : 'State',
+        PostalCode : '123456'
+      }
+    })
+  }
 }
